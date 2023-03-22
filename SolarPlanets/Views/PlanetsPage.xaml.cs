fdd595424c22733bdff515f46573f280998e2c71
SaltyCookiez <vertixo.vider05@gmail.com>
2023-03-22 15:10:51 +0200

    Saving Progress 1

--- SolarPlanets/Views/PlanetsPage.xaml.cs
+++ SolarPlanets/Views/PlanetsPage.xaml.cs
@@ -6,4 +6,17 @@ public partial class PlanetsPage : ContentPage
 	{
 		InitializeComponent();
 	}
+
+	async void GridArea_Tapped(System.Object sender, System.EventArgs e)
+	{
+		await CloseMenu();
+	}
+
+	private async Task CloseMenu()
+	{
+		//close menu and show again main content
+		_ = MainContainer.FadeTo(1, AnimationDuration);
+        _ = MainContentGrid.ScaleTo(1, AnimationDuration);
+		await = MainContentGrid.TranslateTo(0, 0, AnimationDuration);
+    }
 }
